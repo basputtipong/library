0eda22056abfd91d70c0976dade10921a49052d2
bas puttipong <basputtipongz@gmail.com>
2025-04-08 03:09:27 +0700

    add more error res func

--- error/error.go
+++ error/error.go
@@ -1,6 +1,9 @@
 package liberror
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 type HTTPError struct {
 	Code    int    `json:"code"`
@@ -14,4 +17,28 @@ func (e *HTTPError) Error() string {
 
 func New(code int, message string, detail string) *HTTPError {
 	return &HTTPError{Code: code, Message: message, Detail: detail}
+}
+
+func ErrorBadRequest(message string, detail string) *HTTPError {
+	return &HTTPError{Code: http.StatusBadRequest, Message: message, Detail: detail}
+}
+
+func ErrorUnauthorized(message string, detail string) *HTTPError {
+	return &HTTPError{Code: http.StatusUnauthorized, Message: message, Detail: detail}
+}
+
+func ErrorForbidden(message string, detail string) *HTTPError {
+	return &HTTPError{Code: http.StatusForbidden, Message: message, Detail: detail}
+}
+
+func ErrorNotFound(message string, detail string) *HTTPError {
+	return &HTTPError{Code: http.StatusNotFound, Message: message, Detail: detail}
+}
+
+func ErrorInternalServerError(message string, detail string) *HTTPError {
+	return &HTTPError{Code: http.StatusInternalServerError, Message: message, Detail: detail}
+}
+
+func ErrorConflict(message string, detail string) *HTTPError {
+	return &HTTPError{Code: http.StatusConflict, Message: message, Detail: detail}
 }
